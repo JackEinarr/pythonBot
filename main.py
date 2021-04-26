cdc85d93778ceb275bf3ba13d1d6732594a7e501
JacK EinarR <dimon0motamotamt@mail.ru>
2021-04-26 09:28:03 +0300

    hgdfhfg

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 from helpinfo import OutputInterceptor
+from mssqlserver import cnxn, req
 import telebot
 import config
+import schedule
 import tkinter
 import pydoc
 import pygame
+import pyodbc
+import pandas as pd
 
 from telebot import types
 
 bot = telebot.TeleBot(config.token)
-
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
@@ -21,7 +25,6 @@ def welcome(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     item1 = types.KeyboardButton(config.b0[0])
     item2 = types.KeyboardButton(config.b0[1])
-    items = [item1, item2]
     markup.row(item1, item2)
 
     bot.send_message(message.chat.id, config.starttext1.format(message.from_user, bot.get_me()),
@@ -29,29 +32,14 @@ def welcome(message):
 
     bot.send_message(message.chat.id, config.starttext2.format(message.from_user, bot.get_me()),
                      parse_mode='html', reply_markup=markup)
+    #schedule.every(30).seconds.do()
 
 
 @bot.message_handler(content_types=['text'])
 def lalala(message):
-
     if message.chat.type == 'private':
-        if message.text == config.b0[0]:  ##Ответ 'Да'
-
-            bot.send_message(message.chat.id, config.teachtext1.format(message.from_user, bot.get_me()),
-                             parse_mode='html')
-
-            # # keyboard
-            # markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-            # item1 = types.KeyboardButton(config.b6)
-            # item2 = types.KeyboardButton(config.b9)
-            # item4 = types.KeyboardButton(config.b10)
-            #
-            # item3 = types.KeyboardButton(config.b5)
-            # markup.row(item1, item2)
-            # markup.row(item4)
-            # markup.row(item3)
-            # bot.send_message(message.chat.id, config.prisetext1.format(message.from_user, bot.get_me()),
-            #                  parse_mode='html', reply_markup=markup)
+        if message.text == None:        ##Ответ 'Да'
+            pass
         elif message.text == config.b0[1]:  ##Ответ 'Нет'
             bot.send_message(message.chat.id, config.localtext1.format(message.from_user, bot.get_me()),
                              parse_mode='html')
@@ -64,28 +52,40 @@ def lalala(message):
 
             if len(answer) >= 4096:
                 for x in range(0, len(answer), 4096):
-                    bot.send_message(message.chat.id, answer[x:x+4096])
+                    bot.send_message(message.chat.id, answer[x:x + 4096])
             else:
                 bot.send_message(message.chat.id, answer)
 
-# def helpinfo():
-#     @bot.callback_query_handler(func=lambda call: True)
-#     def query_handler(call):
-#         answer = ''
-#         print(call)
-#         if call.data == call.message.text:
-#             with OutputInterceptor() as output:
-#                 help(call.message.text)
-#             answer = '\n'.join(output)
-#             print(answer)
-#
-#         if len(answer[:500]) > 4096:
-#             for x in range(0, len(answer), 4096):
-#                 bot.send_message(call.message.chat.id, answer[x:x + 4096])
-#         else:
-#             bot.send_message(call.message.chat.id, answer[:500].format(call.message.from_user), bot.get_me())
 
-# def outhelp():
+def timer_send_message(message):
+    with open('last_id_update', 'r') as f:  # Достаём код последних данных в базе
+        last_id_update = int(f.read())
+    df = pd.read_sql_query(req + f' WHERE SprStudents.TelegramId={message.chat.id} and Sessiya_id>{last_id_update}',
+                           cnxn)  ##Получаем данные в df по SQL запросу req
+    stud = ' '
+    for columns in df.columns:
+        stud += columns + ' '
+    stud = ' '.join(stud.split())
+    bot.send_message(1296226183, stud, parse_mode='html')
+    stud = ' '
+    for col in df.values:
+        for val in col:
+            stud += str(val) + ' '
 
+        stud = ' '.join(stud.split())
+        bot.send_message(message.chat.id, stud, parse_mode='html')
+        stud = ' '
+    df = pd.read_sql_query('SELECT max([Sessiya_id]) FROM [Dekanat2021].[dbo].[Sessiya]',
+                           cnxn)  ##Получаем код псоледних данных в Sessiya
+    last_id_update = df.values[0][0]
+    # print(last_id_update)
+    print(message.chat.id)
+    with open('last_id_update', 'w') as f:  # Записываем в базу данных новый код обнавления
+        f.write(str(last_id_update))
+
+@bot.message_handler(content_types=None)
+def send_mes(message):
+    timer_send_message(message)
+    print(1)
 # RUN
 bot.polling(none_stop=True)
---
+++ mssqlserver.py
@@ -0,0 +1,13 @@
+import pyodbc
+import pandas as pd
+server_name = '85.142.157.73,8628'
+db_name = 'Dekanat2021'
+User = 'saa'
+password = '322322'
+
+cnxn = pyodbc.connect("Driver={SQL Server Native Client 10.0};"
+                        f"Server={server_name};"
+                        f"Database={db_name};"
+                        f"uid={User};pwd={password}")
+req = f"SELECT SprStudents.Fam, SprStudents.Name, SprStudents.Otch, Sessiya.dateS, SprMarks.mark, SprDisciplines.[Discipline Name] FROM Sessiya INNER JOIN SprStudents ON (Sessiya.StudentId = SprStudents.StudentId) INNER JOIN SprMarks ON Sessiya.id_mark = SprMarks.id_mark INNER JOIN SprDisciplines ON Sessiya.discipline_id = SprDisciplines.discipline_id"
+
